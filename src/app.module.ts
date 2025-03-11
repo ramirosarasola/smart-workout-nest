30bd5c078e5dae6f26a9063ed3483a1e88c74757
ramiro <ramiro.sarasola@gmail.com>
2025-03-11 20:28:09 -0300

    Create new Categories Resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExercisesModule } from './exercises/exercises.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [ExercisesModule],
+  imports: [ExercisesModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
