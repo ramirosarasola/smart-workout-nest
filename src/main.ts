45ef41361e47e5a0160e7d5e9b6b0a566faa29e7
ramiro <ramiro.sarasola@gmail.com>
2025-03-12 19:31:09 -0300

    feat: enhance DTOs with Swagger integration and improve update method error handling

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -19,6 +20,15 @@ async function bootstrap() {
       forbidNonWhitelisted: true, // Retorna 400 si se envia una property que no es esperada.
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
--- src/routines/dto/create-routine.dto.ts
+++ src/routines/dto/create-routine.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsIn,
   IsLowercase,
@@ -9,22 +10,26 @@ import {
 export class CreateRoutineDto {
   @IsString()
   @MinLength(2)
+  @ApiProperty()
   readonly name: string;
 
   @IsString()
   @MinLength(2)
+  @ApiProperty()
   readonly description: string;
 
   @IsString()
   @MinLength(2)
   @IsOptional()
   @IsLowercase()
+  @ApiProperty()
   readonly slug: string;
 
   @IsString()
   @IsIn(['begginer', 'intermediate', 'advanced'])
   @MinLength(2)
   @IsOptional()
+  @ApiProperty()
   readonly level?: string;
 
   // @IsArray()
