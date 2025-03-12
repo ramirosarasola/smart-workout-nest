c9c731add0ac5b5fa28d93b440aa528935548d6f
ramiro <ramiro.sarasola@gmail.com>
2025-03-11 21:14:25 -0300

    Create seed resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExercisesModule } from './exercises/exercises.module';
 import { CategoriesModule } from './categories/categories.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
-  imports: [ExercisesModule, CategoriesModule],
+  imports: [ExercisesModule, CategoriesModule, SeedModule],
   controllers: [AppController],
   providers: [AppService],
 })
