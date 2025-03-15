ae2e0fd2aed7d6464f014fdef5afbd552d7e2167
ramiro <ramiro.sarasola@gmail.com>
2025-03-15 12:38:25 -0300

    Implement JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,38 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { Module, UnauthorizedException } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 import { User } from './entities/user.entity';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+
+    // Lo registramos asyncronicamente, para asegurarnos de que nuestra JWT_SECRET este listo antes de regisrarlo.
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const jwtSecret: string | undefined = configService.get('JWT_SECRET');
+
+        if (!jwtSecret)
+          throw new UnauthorizedException('Missing JWT Secret configurations');
+
+        return {
+          secret: jwtSecret,
+          signOptions: {
+            expiresIn: '1h',
+          },
+        };
+      },
+    }),
+  ],
   exports: [TypeOrmModule],
 })
 export class AuthModule {}
