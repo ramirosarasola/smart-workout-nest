a8e3d841c2a8e26e535a73817ecdd9354a99f501
ramiro <ramiro.sarasola@gmail.com>
2025-03-17 07:22:50 -0300

    Change seed route to POST and add admin authentication

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { SeedService } from './seed.service';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ValidRoles } from 'src/auth/interfaces';
 
 @Controller('seed')
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
-  @Get()
+  @Post()
+  @Auth(ValidRoles.admin)
   runSeed() {
     return this.seedService.populateDB();
   }
--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -5,10 +5,17 @@ import { CategoriesModule } from 'src/categories/categories.module';
 import { RoutinesModule } from 'src/routines/routines.module';
 import { ExercisesModule } from 'src/exercises/exercises.module';
 import { MusclesModule } from 'src/muscles/muscles.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
-  imports: [CategoriesModule, RoutinesModule, ExercisesModule, MusclesModule],
+  imports: [
+    CategoriesModule,
+    RoutinesModule,
+    ExercisesModule,
+    MusclesModule,
+    AuthModule,
+  ],
 })
 export class SeedModule {}
